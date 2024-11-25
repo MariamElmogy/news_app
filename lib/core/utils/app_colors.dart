d03258661f649923f391b6ba3c1e54c795c8f587
MariamElmogy <mariamelmogy3@gmail.com>
2024-11-25 21:18:55 +0200

    - create app colors class

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // L I G H T - T H E M E 
+  static const Color lightBackgroundColor = Color(0XFFFFFFFF);
+  static const Color lightHeadlineColor = Color(0xFF414141);
+  static const Color lightIconColor = Color(0xFF900000);
+  static const Color lightContainerBackground = Color(0xFFFFEFE3);
+  static const Color lightTitleColor = Color(0xFF1E1E1E);
+  static const Color lightSubTitleColor = Color(0xFF8D8D8D);
+  static const Color lightSelectedCategoryContainer = Color(0xFF900000);
+  static const Color lightSelectedCategoryText = Color(0XFFFFFFFF);
+  static const Color lightUnselectedCategoryContainer = Color(0xFFF7F7F7);
+  static const Color lightUnselectedCategoryText = Color(0XFF363636);
+  // D A R K - T H E M E 
+  static const Color darkBackgroundColor = Color(0XFF252728);
+  static const Color darkHeadlineColor = Color(0xFFFFFFFF);
+  static const Color darkIconColor = Color(0xFFE84141);
+  static const Color darkContainerBackground = Color(0xFFFFEFE3);
+  static const Color darkTitleColor = Color(0xFFFFFFFF);
+  static const Color darkSubTitleColor = Color(0xFF8D8D8D);
+  static const Color darkSelectedCategoryContainer = Color(0xFFE84141);
+  static const Color darkSelectedCategoryText = Color(0XFFFFFFFF);
+  static const Color darkUnselectedCategoryContainer = Color(0xFF5A5A5A);
+  static const Color darkUnselectedCategoryText = Color(0XFFBBBBBB);
+}
